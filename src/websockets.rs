fd52ac3b83b1451ece24aee3687c2e51bb7aa4e5
filipton <filipton12@gmail.com>
2023-12-01 21:15:39 +0100

    feat: serde ws input data

--- src/websockets.rs
+++ src/websockets.rs
@@ -1,4 +1,26 @@
-use axum::{extract::{WebSocketUpgrade, ws::WebSocket}, http::Response, body::Body};
+use axum::{
+    body::Body,
+    extract::{ws::WebSocket, WebSocketUpgrade},
+    http::Response,
+};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub struct WsInputData {
+    pub device_id: i128,
+
+    #[serde(flatten)]
+    pub inner: WsInnerData,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(tag = "type", content = "data")]
+#[serde(rename_all = "snake_case")]
+pub enum WsInnerData {
+    Move,
+    Temp { temp: f64, hum: f64 },
+}
 
 pub async fn websocket_handler(ws: WebSocketUpgrade) -> Response<Body> {
     ws.on_upgrade(handle_socket)
@@ -7,6 +29,10 @@ pub async fn websocket_handler(ws: WebSocketUpgrade) -> Response<Body> {
 async fn handle_socket(mut socket: WebSocket) {
     while let Some(msg) = socket.recv().await {
         let msg = if let Ok(msg) = msg {
+            let data = msg.clone().into_data();
+            let data: WsInputData = serde_json::from_slice(&data).unwrap();
+            println!("{:?}", data);
+
             msg
         } else {
             // client disconnected
