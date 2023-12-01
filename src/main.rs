78e65d19c9acb8d091e958b171bdfd85f268d0f5
Bartłomiej Strama <kontakt@bstrama.com>
2023-12-01 14:11:59 +0000

    axum init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use axum::Router;
+use axum::routing::get;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app)
+        .await
+        .expect("Failed to start server on 0.0.0.0:3000");
 }
