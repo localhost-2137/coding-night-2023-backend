cd957a06685f81ed58d7fb268efc78070a1dca40
filipton <filipton12@gmail.com>
2023-12-02 07:12:53 +0100

    feat: dockers etc

--- src/router.rs
+++ src/router.rs
@@ -10,7 +10,6 @@ pub fn router(conn: SqlitePool) -> Router {
         .nest_service("/room", crate::room::router(conn.clone()))
         .nest_service("/auth", crate::auth::router(conn.clone()))
         .nest_service("/schedule", crate::schedule::router(conn.clone()))
-        .route("/ws", get(crate::esp_websockets::websocket_handler))
         .route(
             "/ws/:device_id",
             get(crate::esp_websockets::websocket_handler),
